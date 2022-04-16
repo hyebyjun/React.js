71f663dfeff106fcea42fcc125d4e08e2f1e62cc
hyebyjun <hyebyjun@gmail.com>
2022-04-16 15:25:33 +0900

    useState 간략하게 개념 복습

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState('제목을 입력해주세요.'); // 비구조화 할당
+  const [content, setContent] = useState('내용을 입력해주세요.');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='black-nav'>Blog</div>
+      <div className='list'>
+        <h3>{title}</h3>
+        <p>2022년 4월 17일</p>
+        <p>{content}</p>
+        <hr />
+      </div>
     </div>
   );
 }
