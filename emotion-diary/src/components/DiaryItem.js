3b5d91eb1e6998ef3ef8ae1239dafd14395e5584
hyebyjun <hyebyjun@gmail.com>
2022-05-07 18:06:27 +0900

    감정 점수 재정렬, 일기목록 데이터 전송 구현, 목록 스타일링 마무리

--- emotion-diary/src/components/DiaryItem.js
+++ emotion-diary/src/components/DiaryItem.js
@@ -1,5 +1,38 @@
-const DiaryItem = () => {
-  return <div className='DiaryItem'></div>;
+import MyButton from './MyButton';
+import { useNavigate } from 'react-router-dom';
+
+const DiaryItem = ({ id, emotion, content, date }) => {
+
+  const strDate = new Date(parseInt(date)).toLocaleDateString();
+  
+  const navigate = useNavigate();
+  const goDetail = () => {
+    navigate(`/diary/${id}`);
+  };
+  const goEdit = () => {
+    navigate(`/edit/${id}`);
+  };
+
+  return (
+    <div className='DiaryItem'>
+      <div
+        className={[
+          'emotion_img_wrapper',
+          `emotion_img_wrapper_${emotion}`,
+        ].join(' ')}
+        onClick={goDetail}
+      >
+        <img src={process.env.PUBLIC_URL + `assets/emotion${emotion}.png`} />
+      </div>
+      <div className='info_wrapper'>
+        <div className='diary_date'>{strDate}</div>
+        <div className='diary_content_preview'>{content.slice(0, 25)}</div>
+      </div>
+      <div className='btn_wrapper'>
+        <MyButton text={'수정'} onClick={goEdit} />
+      </div>
+    </div>
+  );
 };
 
 export default DiaryItem;
--- emotion-diary/src/components/DiaryList comments.js
+++ emotion-diary/src/components/DiaryList comments.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import DiaryItem from './DiaryItem';
 import MyButton from './MyButton';
 
 const sortOptionList = [
@@ -16,6 +15,9 @@ const filterOptionList = [
 
 const ControlMenu = ({ value, onChange, optionList }) => {
   return (
+    // value : 컨트롤 메뉴가 렌더링하는 셀렉트가 어떤 걸 선택하고 있는지?
+    // onChange : 셀렉트가 선택하는 게 변화했을 때 바꾸는 함수
+    // optionList : 셀렉트 태그 속에 들어갈 옵션들
     <select
       className='ControlMenu'
       value={value}
@@ -35,10 +37,15 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 const DiaryList = ({ diaryList }) => {
   const navigate = useNavigate();
 
+  // 정렬 기준을 저장할 state
   const [sortType, setSortType] = useState('lastest');
+
+  // 감정 구분해서 필터해줄 상태정보함?ㅎ
   const [filter, setFilter] = useState('all');
 
+  // 정렬 기능을 선택하면 리스트를 기준대로 실제 리스트업해주는 기능
   const getProcessedDiaryList = () => {
+    // [2]
     const compare = (a, b) => {
       if (sortType === 'lastest') {
         return parseInt(b.date) - parseInt(a.date);
@@ -47,7 +54,8 @@ const DiaryList = ({ diaryList }) => {
       }
     };
 
-    const copyList = JSON.parse(JSON.stringify(diaryList));
+    // 실제 리스트 배열에 sort 적용하면 원본 건드리니까, 원본 건들기 지양 ㅇㅇ
+    const copyList = JSON.parse(JSON.stringify(diaryList)); // 이럴 때 깊은 복사 활용해주기
 
     const filterCallBack = (it) => {
       if (filter === 'good') {
@@ -60,7 +68,10 @@ const DiaryList = ({ diaryList }) => {
     const filteredList =
       filter === 'all' ? copyList : copyList.filter((it) => filterCallBack(it));
 
+    // const sortedList = copyList.sort(compare);
+    // 원래 all item이었지만 좋은날/안좋은날 구분하니까 copyList에서 filteredList로 변경
     const sortedList = filteredList.sort(compare);
+    // 정렬하고자 하는 데이터가 현재 배열이므로? 비교함수 만들어준다고? [1]
 
     return sortedList;
   };
@@ -91,14 +102,18 @@ const DiaryList = ({ diaryList }) => {
       </div>
 
       {getProcessedDiaryList().map((it) => (
-        <DiaryItem key={it.id} {...it}/>
+        <div key={it.id}>{it.content}</div>
+        // 키의 의미, 필요성 복습필요합니다.
       ))}
     </div>
   );
 };
 
+// 다이어리 목록에서 프롭이 제대로 전달이 안 될수 있으므로,
 DiaryList.defaultProps = {
   diaryList: [],
 };
+// 이게 뭔 의미래?? 제대로 전달 안 되면 null 오류처럼 될 수 있으니,
+// 데이터 양식? 배열인 거 빈 배열이라도 보험처럼 전달하는거?
 
 export default DiaryList;
