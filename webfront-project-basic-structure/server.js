93b6822b8b487912b7209d6da09d7083179f4f3e
hyebyjun <hyebyjun@gmail.com>
2022-06-01 12:51:43 +0900

    rest api 가져오기 + 프록시 설정해보기?

--- webfront-project-basic-structure/server.js
+++ webfront-project-basic-structure/server.js
@@ -6,8 +6,17 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ message: 'Hello Express!' });
+app.get('/api/customers', (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: 'https://placeimg.com/64/64/1',
+      name: '홍길동',
+      birthday: 1214,
+      genter: '여자',
+      job: '학생',
+    },
+  ]);
 });
 
 app.listen(port, () => console.log(`listening on ${port}`));
