fba443b9cbe652b5dadf953d4de5489c8b5a19a5
hyebyjun <hyebyjun@gmail.com>
2022-06-10 03:25:07 +0900

    useEffect 테스트

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,11 +1,20 @@
 import Button from './Button';
 import styles from './App.module.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log('I run all the time');
+  const iRunOnlyOnce = () => {
+    console.log('i run only once.');
+  };
+  useEffect(iRunOnlyOnce, []); // [] 빈배열은 처음 마운트될 때만 실행되도록.
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
+      <h1 className={styles.title}>{counter}</h1>
       <Button text={'Continue'} />
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
