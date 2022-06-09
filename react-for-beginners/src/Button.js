7e9379bf1b36521e8ad756370acf1fc9e437e7da
hyebyjun <hyebyjun@gmail.com>
2022-06-09 21:14:12 +0900

    styles.moduel.css

--- react-for-beginners/src/Button.js
+++ react-for-beginners/src/Button.js
@@ -1,10 +1,10 @@
 import PropTypes from 'prop-types';
-import styled from './Button.module.css';
+import styles from './Button.module.css';
 
 const Button = ({ text }) => {
   return (
     // <button style={{ backgroundColor: 'tomato', color: 'white' }}>
-    <button className={styled.btn}>{text}</button>
+    <button className={styles.btn}>{text}</button>
   );
 };
 
