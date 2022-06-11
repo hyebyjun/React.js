5c911563fdfdd1c65f291ad55204813637216150
hyebyjun <hyebyjun@gmail.com>
2022-06-11 14:46:37 +0900

    useEffect update

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,19 +1,32 @@
-import Button from './Button';
+// import Button from './Button';
 import styles from './App.module.css';
 import { useEffect, useState } from 'react';
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
   const onClick = () => setValue((prev) => prev + 1);
-  console.log('I run all the time');
-  const iRunOnlyOnce = () => {
-    console.log('i run only once.');
-  };
-  useEffect(iRunOnlyOnce, []); // [] 빈배열은 처음 마운트될 때만 실행되도록.
+  const onChange = (e) => setKeyword(e.target.value);
+  useEffect(() => {
+    console.log('I run only once.');
+  }, []); // [] 빈배열은 처음 마운트될 때만 실행되도록.
+  useEffect(() => {
+    if (keyword !== '' && keyword.length > 5) {
+      console.log('I run when "keyword" changes.', keyword);
+    }
+  }, [keyword]);
+  useEffect(() => {
+    console.log('I run when "counter" changes.');
+  });
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type='text'
+        placeholder='Search Here...'
+      />
       <h1 className={styles.title}>{counter}</h1>
-      <Button text={'Continue'} />
       <button onClick={onClick}>click me</button>
     </div>
   );
