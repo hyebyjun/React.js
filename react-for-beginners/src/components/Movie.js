7e6dc8a1fb6a3daf8a0c17f4229d701ea97e2d8f
hyebyjun <hyebyjun@gmail.com>
2022-06-23 20:32:06 +0900

    home, movie, detail 컴포넌트 및 라우터 분리

---
+++ react-for-beginners/src/components/Movie.js
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+function Movie({ coverImg, title, summary, genres }) {
+  return (
+    <div>
+      <img src={coverImg} alt={title} />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((g) => (
+          <li key={g}>{g}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+// 타입스크립트랑 유사해보이는 프롭타입스..ㅎ..
+Movie.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
