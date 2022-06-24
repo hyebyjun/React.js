92d033370f1d79c99363990a1eeda17439fe3807
hyebyjun <hyebyjun@gmail.com>
2022-06-24 23:07:51 +0900

    router 6 적용 테스트

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path='/hello'>
+          <h1>hello</h1>
+        </Route> */}
+        <Route path='/' element={<Home />} />
+        <Route path='/movie' element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
