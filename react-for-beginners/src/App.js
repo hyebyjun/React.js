754eaf17d9f7ebfb03d569ac26f6448d44f4d15d
hyebyjun <hyebyjun@gmail.com>
2022-06-26 17:18:21 +0900

    .

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,19 +1,19 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Detail from "./routes/Detail";
-import Home from "./routes/Home";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Detail from './routes/Detail';
+import Home from './routes/Home';
 // import './App.css'
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/about-us">
+        <Route path='/about-us'>
           <h1>Hello</h1>
         </Route>
-        <Route path="/movie/:id">
+        <Route path='/movie/:id'>
           <Detail />
         </Route>
-        <Route path="/">
+        <Route path={`${process.env.PUBLIC_URL}/`} element={Home}>
           <Home />
         </Route>
       </Switch>
