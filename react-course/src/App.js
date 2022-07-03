8b3fe9069bb7fa77f000ac47890b1d3f8a28b24b
hyebyjun <hyebyjun@gmail.com>
2022-07-03 23:46:21 +0900

    Link 사용하여 네비바 구성하기

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -2,11 +2,14 @@ import { Route, Switch } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div>
-      <Switch> {/* 스위치 없으면 /와 /이하 가 중첩되어 렌더링된다 */}
+      <MainNavigation />
+      <Switch>
+        {/* 스위치 없으면 /와 /이하 가 중첩되어 렌더링된다 */}
         <Route path='/' exact={true}>
           <AllMeetupsPage />
         </Route>
