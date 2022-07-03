b6fdee47b80861b1b13e7c48a1fc009f5c0ba932
hyebyjun <hyebyjun@gmail.com>
2022-07-03 20:32:23 +0900

    리액트 라우터 5버전 연습해보기

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -1,11 +1,22 @@
-import Todo from './components/Todo';
+import { Route, Switch } from 'react-router-dom';
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritesPage from './pages/Favorites';
 
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text='task 1' />
-      <Todo text='task 2' />
+      <Switch> {/* 스위치 없으면 /와 /이하 가 중첩되어 렌더링된다 */}
+        <Route path='/' exact={true}>
+          <AllMeetupsPage />
+        </Route>
+        <Route path='/new-meetup'>
+          <NewMeetupPage />
+        </Route>
+        <Route path='/favorites'>
+          <FavoritesPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
