3edb11c5ffe9045f5a59c10c0d6311bfb591d4e5
hyebyjun <hyebyjun@gmail.com>
2022-07-06 01:08:14 +0900

    post 요청으로 파베DB 통신 테스트

--- react-course/src/pages/NewMeetup.js
+++ react-course/src/pages/NewMeetup.js
@@ -1,10 +1,20 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+  const onAddMeetupHandler = (meetupData) => {
+    fetch(
+      'https://hyexjun-firebase-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  };
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={onAddMeetupHandler} />
     </section>
   );
 }
