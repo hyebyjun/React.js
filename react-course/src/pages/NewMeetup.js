bcba693216e9abe8921360ebb0a6ab83bb6d41ec
hyebyjun <hyebyjun@gmail.com>
2022-07-06 01:13:46 +0900

    데이터 DB 저장 후 페이지 이동 지정해주기

--- react-course/src/pages/NewMeetup.js
+++ react-course/src/pages/NewMeetup.js
@@ -1,6 +1,9 @@
+import { useHistory } from 'react-router-dom';
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+  const history = useHistory();
+
   const onAddMeetupHandler = (meetupData) => {
     fetch(
       'https://hyexjun-firebase-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
@@ -9,7 +12,9 @@ function NewMeetupPage() {
         body: JSON.stringify(meetupData),
         headers: { 'Content-Type': 'application/json' },
       }
-    );
+    ).then(() => {
+      history.replace('/'); // push 대신 replace 쓰는 이유 : 뒤로가기 해도 의미 없으니 굳이 돌아가지 못하도록 막기
+    }); // fetch, then 대신 await, async 써도 됨
   };
   return (
     <section>
