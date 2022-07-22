90e3b0791c1394a5eff3ff3d299956cd0c94a737
hyebyjun <hyebyjun@gmail.com>
2022-07-22 14:43:51 +0900

    post 요청 간단히 주석

--- react-complete-guide/README.md
+++
@@ -1,4 +0,0 @@
-## React Complete Guide
-
-### 2022.07.01
-- CRA 프로젝트 생성
--- react-course/src/pages/NewMeetup.js
+++ react-course/src/pages/NewMeetup.js
@@ -4,17 +4,20 @@ import NewMeetupForm from '../components/meetups/NewMeetupForm';
 function NewMeetupPage() {
   const history = useHistory();
 
+  // 파베로 db 쓰는 방법
   const onAddMeetupHandler = (meetupData) => {
     fetch(
-      'https://hyexjun-firebase-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json',
+      'https://hyexjun-firebase-default-rtdb.asia-southeast1.firebasedatabase.app/meetups.json', // app/뒤에 원하는 테이블명?.json 입력
       {
-        method: 'POST',
-        body: JSON.stringify(meetupData),
-        headers: { 'Content-Type': 'application/json' },
+        method: 'POST', // 요청 방식
+        body: JSON.stringify(meetupData), // 
+        headers: { 'Content-Type': 'application/json' }, // headers는 필수는 아니다?
       }
-    ).then(() => {
-      history.replace('/'); // push 대신 replace 쓰는 이유 : 뒤로가기 해도 의미 없으니 굳이 돌아가지 못하도록 막기
-    }); // fetch, then 대신 await, async 써도 됨
+    ).then(() => { // fetch, then 대신 await, async 써도 됨
+      history.replace('/'); // push 대신 replace 쓰는 이유
+      // replace는 본래 다른 url로 대체한다는 뜻이지만,
+      // 대체할 이름을 적지 않으면 뒤로가기를 비활성화한다.
+    });
   };
   return (
     <section>
